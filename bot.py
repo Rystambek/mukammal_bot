acf0321021c6639c98f83c87f08342e123789bb0
Rystambek <rustambekjumaqulov@gmail.com>
2023-10-30 11:12:08 +0500

    start polling

--- bot.py
+++ bot.py
@@ -3,9 +3,11 @@ from telegram.ext import Updater, Dispatcher, CommandHandler,   MessageHandler, 
 from handler import start
 
 
-TOKEN = ''
+TOKEN = '6787477123:AAHMi_hfGusB1EUQaAe7m-kYoBgLose9PZc'
 
 updater = Updater(token=TOKEN)
 dp = updater.dispatcher
 
 dp.add_handler(CommandHandler("start",start))
+updater.start_polling()
+updater.idle()
