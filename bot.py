d2800311a1f7644f289430a46e236f1ffd6056ac
Rystambek <rustambekjumaqulov@gmail.com>
2023-10-31 23:18:53 +0500

    tekshir handler

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, Dispatcher, CommandHandler,   MessageHandler, Filters, CallbackContext, CallbackQueryHandler
-from handler import start
+from handler import (start,
+                     tekshir)
 
 
 TOKEN = '6787477123:AAHMi_hfGusB1EUQaAe7m-kYoBgLose9PZc'
@@ -9,5 +10,6 @@ updater = Updater(token=TOKEN)
 dp = updater.dispatcher
 
 dp.add_handler(CommandHandler("start",start))
+dp.add_handler(CallbackQueryHandler(tekshir,pattern='tekshirish'))
 updater.start_polling()
 updater.idle()
